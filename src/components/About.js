b61cb98bbe4d0b2459a0985a3e7f067b9e59a71e
brodarnikola <brodarnikola@gmail.com>
2021-08-18 14:44:14 +0200

    Added correct version into 1.1.0

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 const About = () => {
     return (
         <div>
-            <h4>Version 1.0.0 of SIL ARIEL BRC Application</h4>
+            <h4>Version 1.1.0 of SIL ARIEL BRC Application</h4>
             <Link to='/'>GO BACK</Link>
         </div>
     )
