9c24bc3ccd937fb8e518719d9be5bf23f0183846
brodarnikola <brodarnikola@gmail.com>
2021-08-19 17:20:27 +0200

    Added example of useContext inside this project

--- src/components/CustomHookExample.js
+++ src/components/CustomHookExample.js
@@ -1,13 +1,35 @@
-import { useState, Fragment } from 'react'; 
+import { useState, Fragment } from 'react';
+import React from 'react';
 import Button from './Button';
 import useDataApi from '../customHooks/useDataApi';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import { CurrencyProvider, useCurrency, CURRENCIES } from '../context/contextCurrency';
+
 
 const CustomHookExample = () => {
 
+    // example of useContext 
+    const DATA = [
+        {
+          id: '1',
+          title: 'The Road to React',
+          price: 19.99,
+        },
+        {
+          id: '2',
+          title: 'The Road to GraphQL',
+          price: 29.99,
+        },
+      ];
+
+    const [currency, setCurrency] = React.useState(CURRENCIES.Euro);
+
+
+
     const [query, setQuery] = useState('redux597')
-    const [{data, isLoading, isError}, doFetch] = useDataApi( 'https://hn.algolia.com/api/v1/search?query=redux', { hits: [] })
-   
+    const [{ data, isLoading, isError }, doFetch] = useDataApi('https://hn.algolia.com/api/v1/search?query=redux', { hits: [] })
+
     let htmlContent = ''
     if (isLoading) {
         htmlContent = <div>Loading data... Please wait</div>
@@ -27,10 +49,18 @@ const CustomHookExample = () => {
     return (
         <Fragment>
             <Link to='/'>GO BACK</Link>
+            <br/><br/>
+            <CurrencyProvider>
+                <CurrencyButtons />
+
+                <Books list={DATA} />
+            </CurrencyProvider>
+            <br/>
             <form
                 onSubmit={(event) => {
                     event.preventDefault()
-                    doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`) }}>
+                    doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`)
+                }}>
                 <input
                     type='text'
                     value={query}
@@ -41,6 +71,49 @@ const CustomHookExample = () => {
             {htmlContent}
         </Fragment>
     )
-}
+};
+
+const CurrencyButtons = () => {
+    const { onChange } = useCurrency();
+   
+    return Object.values(CURRENCIES).map((item) => (
+      <CurrencyButton key={item.label} onClick={() => onChange(item)}>
+        {item.label}
+      </CurrencyButton>
+    ));
+  };
+
+const CurrencyButton = ({ onClick, children }) => {
+    return (
+        <button type="button" onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+const Books = ({ list }) => {
+    return (
+        <ul>
+            {list.map((item) => (
+                <Book key={item.id} item={item} />
+            ))}
+        </ul>
+    );
+};
+
+const Book = ({ item }) => {  
+    const { value } = useCurrency();
+
+    const price = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: value.code,
+    }).format(item.price * value.conversionRate);
+
+    return (
+        <li>
+            {item.title} - {price}
+        </li>
+    );
+};
 
 export default CustomHookExample
---
+++ src/context/contextCurrency.js
@@ -0,0 +1,59 @@
+import React from 'react';
+ 
+const CurrencyContext = React.createContext(null);
+ 
+const useCurrency = () => {
+    const [currency, setCurrency] = React.useContext(CurrencyContext);
+   
+    const handleCurrency = (value) => {
+      setCurrency(value);
+    };
+   
+    return { value: currency, onChange: handleCurrency };
+  };
+
+
+const withCurrency = (Component) => (props) => {
+    const currency = useCurrency();
+   
+    return <Component {...props} currency={currency} />;
+  };
+   
+  // if ref is used
+  //
+  // const withCurrency = (Component) =>
+  //   React.forwardRef((props, ref) => {
+  //     const currency = useCurrency();
+   
+  //     return <Component {...props} ref={ref} currency={currency} />;
+  //   });
+   
+
+  const CurrencyProvider = ({ children }) => {
+    const [currency, setCurrency] = React.useState(CURRENCIES.Euro);
+   
+    return (
+      <CurrencyContext.Provider value={[currency, setCurrency]}>
+        {children}
+      </CurrencyContext.Provider>
+    );
+  };
+
+
+  // example of useContext 
+const CURRENCIES = {
+    Euro: {
+        code: 'EUR',
+        label: 'Euro',
+        conversionRate: 1, // base conversion rate
+    },
+    Usd: {
+        code: 'USD',
+        label: 'US Dollar',
+        conversionRate: 1.19,
+    },
+};
+
+
+ 
+export { CurrencyProvider, useCurrency, CURRENCIES };
