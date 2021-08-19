a55ab379aeda747e64f45b8af5b8ee0f2d4dd8ca
brodarnikola <brodarnikola@gmail.com>
2021-08-19 16:04:36 +0200

    Added example of useRef and  useCallBack functions inside project

--- src/components/ReducerExample.js
+++ src/components/ReducerExample.js
@@ -1,15 +1,62 @@
-import { useState, Fragment } from 'react'; 
+import { useState, useRef, useCallback, Fragment } from 'react';
+import React from 'react';
 import Button from './Button';
 import useCustomReducerDataApi from '../customHooks/useCustomReducerDataApi';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid';
+//import List from "./List"
 
 const ReducerExample = () => {
 
-    const baseUrl = 'https://hn.algolia.com/api/v1/search?query='
+    // example of useRef
+    const inputRef = useRef()
+    const focusInput = () => inputRef.current.focus()
 
+    // example of useRef
+    const [counter, setCounter] = useState(0);
+    const counterRef = useRef(counter);
+    const incrementCounter = () => {
+        setCounter(c => c + 1);
+        counterRef.current = counter;
+    }
+    const decrementCounter = () => {
+        setCounter(c => c - 1);
+        counterRef.current = counter;
+    }
+
+
+    // example of useCallback
+    console.log('Render: App');
+    const [users, setUsers] = useState([
+        { id: 'a', name: 'Robin' },
+        { id: 'b', name: 'Dennis' },
+    ]);
+
+    const [text, setText] = useState('');
+
+    const handleText = (event) => {
+        setText(event.target.value);
+    };
+
+    const handleAddUser = () => {
+        setUsers(users.concat({ id: uuidv4(), name: text }));
+        setText('')
+    };
+
+    const handleRemove = useCallback((id) => {
+        console.log("Da li ces sada uci")
+        setUsers(users.filter((user) => user.id !== id));
+    },
+        [users],
+    )
+
+
+    // example of custom reducer. 
+    //this reducer is used to create a backend request to server 
+    const baseUrl = 'https://hn.algolia.com/api/v1/search?query='
     const [query, setQuery] = useState('redux')
-    const [{data, isLoading, isError}, doFetch] = useCustomReducerDataApi( baseUrl+'redux', { hits: [] })
-   
+    const [{ data, isLoading, isError }, doFetch] = useCustomReducerDataApi(baseUrl + 'redux', { hits: [] })
+
     let htmlContent = ''
     if (isLoading) {
         htmlContent = <div>Loading data... Please wait</div>
@@ -17,13 +64,13 @@ const ReducerExample = () => {
     else if (isError && !isLoading) {
         htmlContent = <div style={{ color: 'red' }}>Something went wrong. Please try again latter.</div>
     } else {
-        if( data.hits.length > 0) { 
+        if (data.hits.length > 0) {
             htmlContent = <ul> {data.hits.map(item => (
                 <li key={item.objectID}>
                     <a href={item.url}>{item.title}</a>
                 </li>
             ))}
-        </ul>
+            </ul>
         }
         else {
             htmlContent = <div>There is nothing that macthes this {query} filter. </div>
@@ -33,22 +80,68 @@ const ReducerExample = () => {
     return (
         <Fragment>
             <Link to='/'>GO BACK</Link>
+            <div>
+                <input type="text" value={text} onChange={handleText} />
+                <button type="button" onClick={handleAddUser}>
+                    Add User
+                </button>
+
+                <List list={users} onRemove={handleRemove} />
+            </div>
+            <div>
+                Examples of useRef, to save state variable and html DOM, node object
+                <input type="text" ref={inputRef} />
+                <Button text={'Focus'} onClick={() => focusInput()} />
+            </div>
+            <div>
+                <h1>Counter state: {counter}</h1>
+                <h1 ref={counterRef}>Counter ref: {counter}</h1>
+                <Button text={'+'} onClick={() => incrementCounter()} />
+                <Button text={'-'} onClick={() => decrementCounter()} />
+            </div>
             <form
                 onSubmit={(event) => {
-                    doFetch( baseUrl+ `${query}`)
-                    event.preventDefault() }}>
+                    doFetch(baseUrl + `${query}`)
+                    event.preventDefault()
+                }}>
                 <input
                     type='text'
                     value={query}
                     onChange={(event) => {
                         console.log("hoce li uci sim");
-                        setQuery(event.target.value) }}
+                        setQuery(event.target.value)
+                    }}
                 />
-                <Button text={'Search'} onClick={() => doFetch(baseUrl+ `${query}`)} />
+                <Button text={'Search'} onClick={() => doFetch(baseUrl + `${query}`)} />
             </form>
             {htmlContent}
         </Fragment>
     )
-}
+};
+
+
+const List = React.memo(({ list, onRemove }) => {
+    console.log('Render: List');
+    return (
+        <ul>
+            {list.map((item) => (
+                <ListItem key={item.id} item={item} onRemove={onRemove} />
+            ))}
+        </ul>
+    );
+});
+
+const ListItem = React.memo(({ item, onRemove }) => {
+    console.log('Render: ListItem');
+    return (
+        <li>
+            {item.name}
+            <button type="button" onClick={() => onRemove(item.id)}>
+                Remove
+            </button>
+        </li>
+    );
+});
+
 
 export default ReducerExample
